3c9ba1f483b69beb753148c1677deaa9bc2aac6d
Wisch096 <wisch.pg@outlook.com>
2022-10-29 12:06:34 -0300

    organizando-rotas

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import db from "./config/dbConnect.js";
+import livros from "./models/Livro.js";
+import routes from './routes/index.js';
+
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))
 
@@ -11,18 +14,14 @@ const app = express();
 
 app.use(express.json())
 
-const livros = [
-  {id: 1, "titulo": "Senhor dos Aneis"},
-  {id: 2, "titulo": "O Hobiit"}
-]
+routes(app);
 
-app.get('/', (req, res) => {
-  res.status(200).send('Curso de Node');
-}) 
+// const livros = [
+//   {id: 1, "titulo": "Senhor dos Aneis"},
+//   {id: 2, "titulo": "O Hobiit"}
+// ]
 
-app.get('/livros', (req, res) => {
-  res.status(200).json(livros)
-})
+
 
 app.get('/livros/:id', (req,res) => {
     let index = buscaLivro(req.params.id);
