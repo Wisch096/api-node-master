311bf75bb1a24af1e94a7bf0751f9168541a955e
Wisch096 <wisch.pg@outlook.com>
2022-10-31 14:32:23 -0300

    final

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -3,7 +3,9 @@ import livros from "../models/Livro.js";
 class LivroController {
 
     static listarLivros = (req, res) => {
-        livros.find((err, livros) => {
+        livros.find()
+        .populate('autor')
+        .execute((err, livros) => {
             res.status(200).json(livros)
           })
     }
